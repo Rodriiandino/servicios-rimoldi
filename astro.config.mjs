be471259dd6ea1935b9871efbbac687d2de66158
RodriiAndino <andinorodrigo03@gmail.com>
2025-02-07 14:58:54 -0300

    Add new images, update service and machine card components for responsive design, enhance contact info links, and implement email sending functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   prefetch: true,
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  trailingSlash: 'never'
 })
