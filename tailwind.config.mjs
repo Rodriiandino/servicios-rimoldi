1ec0b2733a1db32121383ac8f36aa2dc730ebb82
RodriiAndino <andinorodrigo03@gmail.com>
2025-02-13 23:26:49 -0300

    Agregar archivos de autenticación y robots, actualizar metadatos y descripciones en varias páginas para mejorar SEO y usabilidad, se añadió FAQ

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,32 +8,32 @@ export default {
         noto: ['Noto Sans Variable', 'sans-serif']
       },
       colors: {
-        'main-color': '#050505',
-        'second-color': '#f1961c',
-        'third-color': '#f8fbfa',
-        'fourth-color': '#777777',
-        'fifth-color': '#0a0a0a',
-        'background-color': '#050505'
+        'main-color': 'var(--main-color)',
+        'second-color': 'var(--second-color)',
+        'third-color': 'var(--third-color)',
+        'fourth-color': 'var(--fourth-color)',
+        'fifth-color': 'var(--fifth-color)',
+        'background-color': 'var(--background-color)'
       },
       maxWidth: {
-        'max-width': 'calc(1250px + 4rem)'
+        'max-width': 'var(--max-width)'
       },
       padding: {
-        side: '2rem',
-        'side-sm': '1rem'
+        side: 'var(--padding-side)',
+        'side-sm': 'var(--padding-side)'
       },
       borderRadius: {
-        '10px': '10px'
+        '10px': 'var(--border-radius)'
       },
       fontSize: {
-        xxs: 'clamp(0.7813rem, 0.7747rem + 0.0326vw, 0.8rem)',
-        xs: 'clamp(0.9375rem, 0.9158rem + 0.1087vw, 1rem)',
-        0: 'clamp(1.125rem, 1.0815rem + 0.2174vw, 1.25rem)',
-        1: 'clamp(1.35rem, 1.2761rem + 0.3696vw, 1.5625rem)',
-        2: 'clamp(1.62rem, 1.5041rem + 0.5793vw, 1.9531rem)',
-        3: 'clamp(1.944rem, 1.771rem + 0.8651vw, 2.4414rem)',
-        4: 'clamp(2.3328rem, 2.0827rem + 1.2504vw, 3.0518rem)',
-        5: 'clamp(2.7994rem, 2.4462rem + 1.7658vw, 3.8147rem)'
+        xxs: 'var(--step--2)',
+        xs: 'var(--step--1)',
+        0: 'var(--step-0)',
+        1: 'var(--step-1)',
+        2: 'var(--step-2)',
+        3: 'var(--step-3)',
+        4: 'var(--step-4)',
+        5: 'var(--step-5)'
       }
     }
   },
